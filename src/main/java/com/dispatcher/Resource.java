8e9e48b83acdde8e48a6416aa7ae7f9881775037
ismail Ensar GULSEN <ismail_ensar.gulsen@toyotatr.com>
2023-12-29 20:41:18 +0300

    resource written

--- src/main/java/com/dispatcher/Resource.java
+++ src/main/java/com/dispatcher/Resource.java
@@ -1,5 +1,47 @@
 package main.java.com.dispatcher;
 
 public class Resource {
-    
+    public static Integer REMAINING_CD_COUNT = 2;
+    public static Integer REMAINING_MODEM_COUNT = 1;
+    public static Integer REMAINING_PRINTER_COUNT = 2;
+    public static Integer REMAINING_SCANNER_COUNT = 1;
+    public static Integer REMAINING_MEMORY_SIZE = 1024;
+
+    /**
+     * Allocate resources to process
+     * 
+     * @return true if resources were allocated, false otherwise
+     */
+    public static boolean allocate(Process process) {
+        if (REMAINING_CD_COUNT >= process.getCdCount()
+                && REMAINING_MODEM_COUNT >= process.getModemCount()
+                && REMAINING_PRINTER_COUNT >= process.getPrinterCount()
+                && REMAINING_SCANNER_COUNT >= process.getScannerCount()
+                && REMAINING_MEMORY_SIZE >= process.getMemorySize()) {
+            REMAINING_CD_COUNT -= process.getCdCount();
+            REMAINING_MODEM_COUNT -= process.getModemCount();
+            REMAINING_PRINTER_COUNT -= process.getPrinterCount();
+            REMAINING_SCANNER_COUNT -= process.getScannerCount();
+            REMAINING_MEMORY_SIZE -= process.getMemorySize();
+
+            process.setId(System.currentTimeMillis());
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean deallocate(Process process) {
+        if (process.getId() == null) {
+            return false;
+        }
+        
+        REMAINING_CD_COUNT += process.getCdCount();
+        REMAINING_MODEM_COUNT += process.getModemCount();
+        REMAINING_PRINTER_COUNT += process.getPrinterCount();
+        REMAINING_SCANNER_COUNT += process.getScannerCount();
+        REMAINING_MEMORY_SIZE += process.getMemorySize();
+
+        return true;
+    }
 }
